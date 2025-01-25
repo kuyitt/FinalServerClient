ac5c0a6c71e593dd9d860309836f729e3a57a07d
kutiyt <Franklintomr@gmail.com>
2025-01-25 18:46:09 +0000

    Add project files.

---
+++ FinalServerClient/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalServerClient
+{
+    public class Client
+    {
+        public Client()
+        {
+
+        }
+        public void Connect()
+        {
+            int port = 16000;
+            string IP = "192.168.0.23";
+
+            using TcpClient client = new TcpClient(IP, port);
+        }
+    }  
+}
---
+++ FinalServerClient/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+
+namespace FinalServerClient
+{
+    public class Run
+    {
+        public static void Main()
+        {
+            var server = new Server();
+        }
+    }
+    public class Server
+    {
+        public Server() 
+        {
+            string hostName = Dns.GetHostName();
+            string IP = Dns.GetHostEntry(hostName).AddressList[0].ToString();
+            Console.WriteLine(hostName + IP);
+            IPAddress listenerIP = IPAddress.Parse(IP);
+            int port = 16000;
+            int bufferSize = 256;
+
+            TcpListener listener = new TcpListener(listenerIP,port);
+
+            listener.Start();
+
+            
+
+
+
+
+        }
+        public void Initiate()
+        {
+
+        }
+        //public bool Needed()
+        //{
+
+        //}
+        public void GetLocalInfo()
+        {
+            
+        }
+    }
+}
